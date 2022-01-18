9d04ae965961b14b6149d1d6e78c9194e75e9f39
Ayman Ali <63648492+aym183@users.noreply.github.com>
2022-01-18 20:17:50 +0100

    cleaned out code

--- DFS.java
+++ DFS.java
@@ -24,32 +24,22 @@ public class F012800Source {
 		
 		for (int value: Indexes) {
 			if(value == input - 3 ) {
-				
 				possible_basemoves.add("Up");
-				
 			}
 			else if(value == input + 3 ) {
-				
 				possible_basemoves.add("Down");
-				
 			}
 			else if(value == input - 1 ) {
-				
 				possible_basemoves.add("Left");
-				
 			}
 			else if(value == input + 1 ) {
-				
 				possible_basemoves.add("Right");
-			
 			}
-			
 			else {
 				continue;
 			}
 		}
 	}
-	
 	
 	// Method to implement possible moves on the input node
 	
@@ -65,8 +55,8 @@ public class F012800Source {
 			;
 		}else {
 			base_nodes.add(Input);
-		}
-		
+        }
+
 		// Operation to check what the index of the empty block is
 		
 		for(int i = 0; i< Actual_Input.length; i++) {
@@ -96,28 +86,22 @@ public class F012800Source {
 				temp = c[Index];
 				c[Index] = c[Index - 3];
 				c[Index - 3] = temp;
-				
 				covered_word = "";
 				String new_word = new String(c);
 				
 				// If move has already been covered, wont be covered again
 				if(covered_nodes.contains(new_word)) {
-					
 					continue;
 				}
 				else {
-					
 					/* After each operation, all the possible basemoves are cleared and the method is run again
 					 If only one possible move, nothing will be done. If multiple states, code will just skip over covered state till 
 					 all are covered */
 						
-						
 						possible_basemoves.clear();
 						covered_nodes.add(new_word);
 						all_nodes(Input);
-				
-				}	
-				
+				}
 			}
 			
 			// State implementation when possible move is Down
@@ -126,16 +110,14 @@ public class F012800Source {
 				for(int i1 = 0; i1< Actual_Input.length; i1++) {
 					covered_word = covered_word + Actual_Input[i1];
 				}
-				char[] c = covered_word.toCharArray();
-				
-				temp = c[Index];
-				c[Index] = c[Index + 3];
-				c[Index + 3] = temp;
-				
-				covered_word = "";
-				String new_word = new String(c);
+                    char[] c = covered_word.toCharArray();
+                    temp = c[Index];
+                    c[Index] = c[Index + 3];
+                    c[Index + 3] = temp;
+                    covered_word = "";
+                    String new_word = new String(c);
+
 				if(covered_nodes.contains(new_word)) {
-					
 					continue;
 				}
 				else {
@@ -153,30 +135,26 @@ public class F012800Source {
 				}
 				
 				// since 3 or 6 is on the left edge and cannot move left to index 2 or 5, it isnt covered
-				if(Index == 3 || Index == 6) {
-					
+				if(Index == 3 || Index == 6) {	
 					covered_word = "";
 				}
 				else {
-				char[] c = covered_word.toCharArray();
-				
-				temp = c[Index];
-				c[Index] = Actual_Input[Index - 1];
-				c[Index - 1] = temp;
-				
-				covered_word = "";
-				String new_word = new String(c);
-				if(covered_nodes.contains(new_word)) {
-					
-					continue;
-				}
-			
-				else {
-					possible_basemoves.clear();
-					
-					covered_nodes.add(new_word);
-					all_nodes(Input);
-				}		
+                    char[] c = covered_word.toCharArray();
+                    temp = c[Index];
+                    c[Index] = Actual_Input[Index - 1];
+                    c[Index - 1] = temp;
+                    covered_word = "";
+                    String new_word = new String(c);
+
+                    if(covered_nodes.contains(new_word)) {
+                        
+                        continue;
+                    }
+                    else {
+                        possible_basemoves.clear();
+                        covered_nodes.add(new_word);
+                        all_nodes(Input);
+                    }		
 				}		
 			}
 			
@@ -189,41 +167,32 @@ public class F012800Source {
 				
 				// since 2 or 5 is on the right edge and cannot move right to index 3 or 6, it isnt covered
 				if(Index == 2 || Index == 5) {
-					
 					covered_word = "";     
 				}
 				else {
-					
-				char[] c = covered_word.toCharArray();
-				
-				temp = c[Index];
-				c[Index] = c[Index + 1];
-				c[Index + 1] = temp;
-				covered_word = "";
-				String new_word = new String(c);
+                    char[] c = covered_word.toCharArray();
+                    temp = c[Index];
+                    c[Index] = c[Index + 1];
+                    c[Index + 1] = temp;
+                    covered_word = "";
+                    String new_word = new String(c);
 				 
-				if(covered_nodes.contains(new_word)) {
-					
-					continue;
-				}
-				else {
-					
-					possible_basemoves.clear();
-					covered_nodes.add(new_word);
-					all_nodes(Input);
-				}
-				
+                    if(covered_nodes.contains(new_word)) {
+                        continue;
+                    }
+                    else { 
+                        possible_basemoves.clear();
+                        covered_nodes.add(new_word);
+                        all_nodes(Input);
+                    }
 				}				
 			}
 			
 			else {
 				continue;
-			}
-			
+			}	
 		}
-		
 		possible_basemoves.clear();
-		
 	}
 	
 	
@@ -236,21 +205,17 @@ public class F012800Source {
 		                                                 
 		Stack<String> S = new Stack<String>();
 		S.push(s);   
-
 		File output_file = new File("output.txt");
 		FileWriter fstream = new FileWriter("output.txt");
 		BufferedWriter info = new BufferedWriter(fstream);
 		
 		// each node that is pushed into the stack will be put into a hashmap as visited
-		visited_nodes.put(s, true);
-		
-		
+		visited_nodes.put(s, true);		
 		// while loop to traverse through depth of each node. Runs till stack is empty
 		
 		while(!S.empty()) {
 			
 			// Takes the first element from the stack and pops it
-			
 			String u = S.peek();
 			S.pop();
 			all_nodes(u);	//method to get the possible states	
@@ -258,26 +223,18 @@ public class F012800Source {
 			info.newLine();
 			
 			for(String v: covered_nodes ){
-				
 				// checking to see  whether state has already been covered
-				
 				if(visited_nodes.containsKey(v)) {
 					continue;
 				}
-				
 				else {
 					S.push(v);
-					
 					visited_nodes.put(v, true);
-					
 				}
-				
 			}
 			covered_nodes.clear();
-		
 		}
 		info.close();
-		
 	}
 	
 	// Method to check common states between R(S1) and R(S2)
@@ -315,12 +272,10 @@ public class F012800Source {
 		BufferedWriter info = new BufferedWriter(fstream);
 		
 		for(String i: states_intersection) {
-		
 			info.write(i);
 			info.newLine();
 		}
 		info.close();
-		
 	}
 	
 	// Console interface for user to interact with system
@@ -339,7 +294,6 @@ public class F012800Source {
 		System.out.println("d -> The length of reachable states of S2");
 		System.out.println("e -> All reachable states common between S1 and S2");
 		System.out.println("f -> The length of common reachable states between S1 and S2");
-		
 		System.out.println("");
 		
 		Scanner sc=new Scanner(System.in);  
@@ -350,13 +304,11 @@ public class F012800Source {
 		
 		if(response == 'a') {
 			
-			
 			DFS_it(Input1);
 			System.out.println("a) The reachable states of " + "\"" + Input1 + "\"" + " are printed into the text file.");
 			System.out.println("The number of reachable states for " + "\""+ Input2 + "\"" +  " is: " + visited_nodes.size());
 			visited_nodes.clear();
 			covered_nodes.clear();
-			
 			System.out.println("------");
 			
 			// To check whether the user wants to continue with other operations
@@ -365,24 +317,20 @@ public class F012800Source {
 			
 			char continuation_response = sc.next().charAt(0); 
 			if(continuation_response == 'Y') {
-				
 				Program(Input1, Input2);
-				
 			}
 			else if(continuation_response == 'N') {
 				System.out.println("Have a good day!");
 				System.exit(0);
-				
 			}
 			
 			// Error checking
 			else {
 				System.out.println("Invalid!");
 				System.exit(0);
-				
 			}
-			
 		}
+
 		else if(response == 'b') {
 			
 			DFS_it(Input1);
@@ -394,51 +342,42 @@ public class F012800Source {
 			
 			char continuation_response = sc.next().charAt(0); 
 			if(continuation_response == 'Y') {
-				
 				Program(Input1, Input2);
-				
 			}
 			else if(continuation_response == 'N') {
 				System.out.println("Have a good day!");
 				System.exit(0);
-				
 			}
 			else {
 				System.out.println("Invalid!");
 				System.exit(0);
 			}
-			
 		}
+
 		else if(response == 'c') {
 			
 			DFS_it(Input2);
 			System.out.println("c) The reachable states of " + "\"" + Input2 + "\"" + " are printed into the text file.");
 			System.out.println("The number of reachable states for " + "\""+ Input2 + "\"" +  " is: " + visited_nodes.size());
-			
 			visited_nodes.clear();
 			covered_nodes.clear();
-			
 			System.out.println("------");
 			System.out.println("Do you want to continue? Y -> Yes or N -> No");
-			
 			char continuation_response = sc.next().charAt(0); 
+
 			if(continuation_response == 'Y') {
-				
 				Program(Input1, Input2);
-				
 			}
 			else if(continuation_response == 'N') {
 				System.out.println("Have a good day!");
 				System.exit(0);
-				
 			}
 			else {
 				System.out.println("Invalid!");
 				System.exit(0);
-			}
-			
-					
+            }	
 				}
+            
 		else if(response == 'd') {
 			
 			DFS_it(Input2);
@@ -450,21 +389,20 @@ public class F012800Source {
 			
 			char continuation_response = sc.next().charAt(0); 
 			if(continuation_response == 'Y') {
-				
 				Program(Input1, Input2);
 				
 			}
 			else if(continuation_response == 'N') {
-				
+                System.out.println("Have a good day!");
+				System.exit(0);
 			}
 			else {
 				System.out.println("Invalid!");
 				System.exit(0);
 			}
-			
 		}
+
 		else if(response == 'e') {
-			
 			
 			intersection_of_states(Input1, Input2);
 			System.out.println("e) The reachable states of " + "\"" + Input1 + "\"" + " and " + "\"" + Input2 + "\"" + " are printed into the text file.");
@@ -474,9 +412,7 @@ public class F012800Source {
 			
 			char continuation_response = sc.next().charAt(0); 
 			if(continuation_response == 'Y') {
-				
 				Program(Input1, Input2);
-				
 			}
 			else if(continuation_response == 'N') {
 				System.out.println("Have a good day!");
@@ -486,35 +422,27 @@ public class F012800Source {
 				System.out.println("Invalid!");
 				System.exit(0);
 			}
-			
 		}
+
 		else if(response == 'f') {
 			
 			intersection_of_states(Input1, Input2);
 			System.out.println("f) The number of reachable states for " + "\"" + Input1 + "\""+ " and " + "\"" + Input2 + "\""  + "  is "  + states_intersection.size());
-			
 			System.out.println("------");
 			System.out.println("Do you want to continue? Y -> Yes or N -> No");
 			
 			char continuation_response = sc.next().charAt(0); 
 			if(continuation_response == 'Y') {
-				
 				Program(Input1, Input2);
-				
 			}
 			else if(continuation_response == 'N') {
-				
 				System.out.println("Have a good day!");
 				System.exit(0);
-				
-			}
-			
+            }
 			else {
-				
 				System.out.println("Invalid!");
 				System.exit(0);
 			}
-			
 		}
 		
 		// Error checking
@@ -525,8 +453,6 @@ public class F012800Source {
 		}
 	
 	}
-	
-	
 	
 	/* Main method where base inputs are entered. If user wants to run their own input,
 	 * please replace the inputs below. Please enter a space at the index you would like the empty block to be located
@@ -563,38 +489,27 @@ public class F012800Source {
 				sc.nextLine();  
 			
 				for (int i = 0; i < string.length; i++)   { 
-				
 					string[i] = sc.nextLine();  
-					
 				}  
 			
 				input1 = string[0];
 				input2 = string[1];
 				Program(input1, input2);
-			
 			}
 			
 			else {
 				
 				System.out.println("Please enter 2!");
 				System.exit(0);
-				
-			}
-			
-			
+			}			
 		}
 		
 		else if(response == 'S'){
-			
 			Program(input1, input2);	
-			
 		}
 		else {
 			System.out.println("Invalid!");
 			System.exit(0);
-		}
-		
-		
-		
+		}		
 	}
 		}
